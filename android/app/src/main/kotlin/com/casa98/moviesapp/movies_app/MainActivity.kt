84af5921d62bba8a27325f811212a3771d8ac515
Carlos Sánchez <cagudeloa@uninorte.edu.co>
2022-01-17 09:52:45 -0500

    Add one basic screen

---
+++ android/app/src/main/kotlin/com/casa98/moviesapp/movies_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.casa98.moviesapp.movies_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/screens.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomeScreen(),
+        'detail': (_) => DetailsScreen(),
+      },
+    );
+  }
+}
