2affe936c787688693be8c9b9f12e5df8558c4f9
Carlos Sánchez <cagudeloa@uninorte.edu.co>
2022-01-20 10:49:19 -0500

    Create models and parse data from api

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/providers/movies_privder.dart';
+import 'package:provider/provider.dart';
 import 'screens/screens.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      child: const MyApp(),
+      providers: [
+        ChangeNotifierProvider(create: (_) => MoviesProvider(), lazy: false)
+      ],
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
